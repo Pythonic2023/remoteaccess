ce13d267088824dda942e1935c06e3e0a9c82faa
Pythonic2023 <daltonfaust7295@gmail.com>
2024-10-29 13:12:08 -0400

    Added quick and dirty listening socket for testing purposes. Added some usage messages to the description of help message.

--- slither.py
+++ slither.py
@@ -8,19 +8,41 @@ import socket
 import sys
 import argparse
 
+
+# TODO: 2) Function to execute on depending on options, create RAT socket and call to client or create the client socket
+"""Function for rat to initialize and call out for the controller, for now don't call out to controller"""
+
+
+def remote_sock():
+    address = '127.0.0.1'
+    port = 9000
+
+    rat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rat_socket.bind((address, port))
+    print(f"[*] Bound to {address, port}")
+    rat_socket.listen()
+    print(f"[*] Listening")
+    client_sock, addr = rat_socket.accept()
+    client_msg = client_sock.recv(1024)
+    print(client_msg.decode())
+
+
 # TODO: 1) Check if we are the RAT or client, execute program accordingly.
 parser = argparse.ArgumentParser(
     prog='slither.py',
-    description='Remote Access Tool'
+    formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        [*] SLITHER [*]
+        
+        Options:
+        [*] -r for rat, remote instance for controlling
+        [*] -c for controller, gets access to shell from rat
+    '''
 )
 parser.add_argument('-r', '--rat', action='store_true')
 parser.add_argument('-c', '--control', action='store_true')
 
 
 args = parser.parse_args()
-print(args)
-
-# TODO: 2) Functions to execute on above results, create RAT socket and call to client or create the client socket.
 
 # TODO: 3) If RAT, create a shell and provide it to the client
 
